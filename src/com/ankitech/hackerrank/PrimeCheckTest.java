f1e2d61e85c6ca6d812267fb9e65e626b814ba7c
kumar ankit <ankitech@gmail.com>
2019-08-14 00:41:35 +0530

    Java Primality Test
    https://www.hackerrank.com/challenges/java-primality-test/problem

---
+++ src/com/ankitech/hackerrank/PrimeCheckTest.java
@@ -0,0 +1,17 @@
+package com.ankitech.hackerrank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PrimeCheckTest {
+
+    @Test
+    public void isPrime() {
+        assertTrue(PrimeCheck.isPrime(13));
+        assertTrue(PrimeCheck.isPrime(17));
+        assertFalse(PrimeCheck.isPrime(9));
+        assertFalse(PrimeCheck.isPrime(32));
+    }
+}
